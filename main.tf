db49a8531ca975e33440f06e26bb9c9442751b0a
Mohammad Shaik <mmtech360@gmail.com>
2026-06-12 11:11:50 +1000

    Added cloud infra resources

--- main.tf
+++ main.tf
@@ -14,3 +14,118 @@ resource "azurerm_resource_group" "main" {
   location = var.location
   tags     = local.tags
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${local.name_prefix}-aue"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.tags
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "snet-web-${local.name_prefix}-aue"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_public_ip" "lb" {
+  name                = "pip-${local.name_prefix}-aue"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = local.tags
+}
+
+resource "azurerm_lb" "main" {
+  name                = "lb-${local.name_prefix}-aue"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+  tags                = local.tags
+
+  frontend_ip_configuration {
+    name                 = "fe-public"
+    public_ip_address_id = azurerm_public_ip.lb.id
+  }
+}
+
+
+resource "azurerm_lb_backend_address_pool" "web" {
+  name            = "bepool-web"
+  loadbalancer_id = azurerm_lb.main.id
+}
+
+resource "azurerm_lb_probe" "http" {
+  name            = "probe-http"
+  loadbalancer_id = azurerm_lb.main.id
+  protocol        = "Http"
+  port            = 80
+  request_path    = "/"
+}
+
+resource "azurerm_lb_rule" "http" {
+  name                           = "rule-http"
+  loadbalancer_id                = azurerm_lb.main.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "fe-public"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.web.id]
+  probe_id                       = azurerm_lb_probe.http.id
+}
+
+
+
+resource "azurerm_linux_virtual_machine_scale_set" "web" {
+  name                = "vmss-${local.name_prefix}-aue"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = var.vm_size
+  instances           = var.instance_count
+  admin_username      = var.admin_username
+  upgrade_mode        = "Automatic"
+
+  custom_data = base64encode(file("${path.module}/cloud-init.yaml"))
+
+  tags = local.tags
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/mo-public.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  network_interface {
+    name    = "nic-web"
+    primary = true
+
+    ip_configuration {
+      name                                   = "ipconfig-web"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.web.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web.id]
+    }
+  }
+
+  health_probe_id = azurerm_lb_probe.http.id
+
+  automatic_instance_repair {
+    enabled      = true
+    grace_period = "PT10M"
+  }
+}
+
